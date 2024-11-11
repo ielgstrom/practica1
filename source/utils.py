ea93acf3a49561a082fb07444b086270f99023ef
Ignasi Elgström <i.elgstrom@gmail.com>
2024-11-10 21:13:07 -0500

    refactor

--- source/utils.py
+++ source/utils.py
@@ -1,7 +1,8 @@
+import requests
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
-from bs4 import BeautifulSoup as BS
+from bs4 import BeautifulSoup as bs
 import requests as rq
 import pandas as pd
 
@@ -18,13 +19,13 @@ def return_js_page(url):
         driver.get(url)
         result = driver.page_source
         driver.quit()
-        return BS(result, features="html.parser")
+        return bs(result, features="html.parser")
     except Exception as e:
         print(e)
         return ""
 
 
-def get_list_of_nav():
+def get_list_of_nav() -> dict[str:str]:
     url = 'https://datosmacro.expansion.com'
     page = smart_get_request(url)
     nav = page.find(id='block-bstb5-dm-topmenu').find_all('li')
@@ -32,7 +33,8 @@ def get_list_of_nav():
     return list_of_nav
 
 
-def generate_result_table(country_links, list_headers_table=[0, 1, 2], list_header_table=[0, 1, 2], link_definition=lambda x:x):
+def generate_result_table(country_links, list_headers_table=[0, 1, 2], list_header_table=[0, 1, 2],
+                          link_definition=lambda x: x) -> pd.DataFrame:
     df_result = pd.DataFrame()
     for country, link in country_links.items():
         link = link_definition(link)
@@ -45,15 +47,15 @@ def generate_result_table(country_links, list_headers_table=[0, 1, 2], list_head
     return df_result
 
 
-def get_smi_yearly_data():
+def get_smi_yearly_data() -> pd.DataFrame:
     url_smi = get_list_of_nav()['Salario SMI']
     country_links_smi = get_country_links(url_smi)
     df_result = generate_result_table(country_links_smi, link_definition=lambda x: url_smi + '/' + x.split('/')[2])
     df_result['SMI'] = pd.to_numeric(df_result['SMI'].str.replace('$', '', regex=False)
-                                                 .str.replace('.', '', regex=False)
-                                                 .str.replace(',', '.', regex=False)
-                                                 .str.replace(' ', '', regex=False)
-                                                 .str.replace('\u00A0', '', regex=False))
+                                     .str.replace('.', '', regex=False)
+                                     .str.replace(',', '.', regex=False)
+                                     .str.replace(' ', '', regex=False)
+                                     .str.replace('\u00A0', '', regex=False))
     df_result['SMI'] = df_result['SMI'].round(2)
     df_result['SMI Mon. Local'] = pd.to_numeric(df_result['SMI Mon. Local']
                                                 .str.replace('.', '', regex=False)
@@ -67,7 +69,7 @@ def get_smi_yearly_data():
     return df_result
 
 
-def get_deficit_yearly_data():
+def get_deficit_yearly_data() -> pd.DataFrame:
     country_links_deficit = get_country_links(get_list_of_nav()['Déficit'])
     df_result = generate_result_table(country_links_deficit, [0, 1, 3], [0, 1, 3])
     df_result['Déficit (%PIB)'] = pd.to_numeric(df_result['Déficit (%PIB)'].str.replace(',', '.', regex=False)
@@ -83,16 +85,16 @@ def get_debt_yearly_data():
     df_result = generate_result_table(country_links_debt, [0, 1, 3, 4], [0, 1, 3, 4])
     df_result['Deuda total (M.€)'] = pd.to_numeric(df_result['Deuda total (M.€)'].str.replace('.', '', regex=False))
     df_result['Deuda (%PIB)'] = pd.to_numeric(df_result['Deuda (%PIB)'].str.replace(',', '.', regex=False)
-                                                .str.replace('%', '', regex=False))
+                                              .str.replace('%', '', regex=False))
     df_result['Deuda Per Cápita'] = pd.to_numeric(df_result['Deuda Per Cápita'].str.replace('.', '', regex=False)
-                                              .str.replace('€', '', regex=False)
+                                                  .str.replace('€', '', regex=False)
                                                   .str.replace('\u00A0', '', regex=False))
-    df_result.rename(columns={'Deuda total (M.€)': 'Deute Total', 'Deuda (%PIB)': 'Deute PIB','Deuda Per Cápita':'Deute Per Capita'}, inplace=True)
+    df_result.rename(columns={'Deuda total (M.€)': 'Deute Total', 'Deuda (%PIB)': 'Deute PIB',
+                              'Deuda Per Cápita': 'Deute Per Capita'}, inplace=True)
     return df_result
 
 
-def get_epa_yearly_data():
-
+def get_epa_yearly_data() -> pd.DataFrame:
     country_links_epa = get_country_links(get_list_of_nav()['EPA'])
     df_result = generate_result_table(country_links_epa, [0, 1, 2, 3], [0, 1, 2, 3])
     df_result['Fecha'] = df_result['Fecha'].str.replace(r'[^0-9.]', '', regex=True)
@@ -132,14 +134,14 @@ def get_table_data(url_to_search: str, list_headers_table: list, list_columns_ta
         return pd.DataFrame()
 
 
-def get_country_links(url):
+def get_country_links(url) -> dict[str, str]:
     smi_page = smart_get_request(url)
     table = smi_page.find('table').tbody.find_all('tr')
     list_of_countries = {row.td.string.replace(' [+]', ''): row.td.a['href'] for row in table}
     return list_of_countries
 
 
-def smart_get_request(url, timeout_seconds=15):
+def smart_get_request(url: str, timeout_seconds: int = 15):
     headers = {
         "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,\
         */*;q=0.8",
@@ -152,10 +154,9 @@ def smart_get_request(url, timeout_seconds=15):
         "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_3) AppleWebKit/5\
         37.36 (KHTML, like Gecko) Chrome/56.0.2924.87 Safari/537.36"
     }
-    data = ''
     try:
         r = rq.get(url, headers=headers, timeout=timeout_seconds)
-        data = BS(r.content, features="html.parser")
+        data = bs(r.content, features="html.parser")
     except rq.exceptions.Timeout:
         print("Request a {} ha trigat massa en resoldre".format(url))
         pass
